c9fdb4fc558bfb77724eec644b6feb227ceca1c4
Cat <Cat@CatBook.local>
2015-12-16 21:05:06 -0500

    read more story and resize header

--- scripts/upload_local.sh
+++ scripts/upload_local.sh
@@ -1,6 +1,6 @@
 #yes local is 3001
 
 TODAY=$(date +"%d-%m-%Y") &&
-mongoimport -h localhost:3031 --db meteor --collection articles --type json --file data/$TODAY/$TODAY-CLEAN.json --jsonArray &&
+mongoimport -h localhost:3001 --db meteor --collection articles --type json --file data/$TODAY/$TODAY-CLEAN.json --jsonArray &&
 echo 'imported files to local'  
  
